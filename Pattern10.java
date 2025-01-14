1bb8ed762e0154eb4b268028fedd63aac144ddaf
Anshu Kushwaha <anshukushwaha112@gmail.com>
2025-01-14 07:02:33 +0530

    Pattern10.java

--- Pattern10.java
+++ Pattern10.java
@@ -4,7 +4,7 @@ public class Pattern10 {
 
     void Pattern(int n){
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < i+1; j++) {
                 System.out.print("*");
             }
             System.out.println();

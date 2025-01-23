3ae2446c0e46867508d9820b8acf094d3ba29e02
Anshu Kushwaha <anshukushwaha112@gmail.com>
2025-01-23 09:07:55 +0530

    Add files via upload

---
+++ Pattern21.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern21 {
+    void Pattern(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n-1 || j == n-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of rows: ");
+        int n = sc.nextInt();
+        Pattern21 p = new Pattern21();
+        p.Pattern(n);
+    }
+}
